abbb62686c29175a7af869123f6fb5c5469436fa
David Ikin <shadow2272@aol.com>
2021-05-23 14:59:07 +0100

    PEP8 Style

--- pyroulette.py/pyroulette.py
+++ pyroulette.py/pyroulette.py
@@ -1,19 +1,20 @@
 # pyroulette in development. dave ikin 2021
-import sys
+# roulette simulator
+
+from sys import exit
 from random import choice
 
 def roulette():
     numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,
                19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
-    results = []
+    
     x = choice(numbers)
-    results.append(x)
     print(f' {x} wins.')
-    print(results)
+    
     again = input('Again? y/n')
     if again == 'y' or again == 'Y':
         roulette()
-    else: sys.exit()
+    else: exit()
 
 
 roulette()
